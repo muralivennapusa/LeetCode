ca50e4b06004c71bb6abc852d8607b6edb90a0ca
Murali Vennapusa <146339887+muralivennapusa@users.noreply.github.com>
2025-11-03 05:33:05 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 1578-minimum-time-to-make-rope-colorful/1578-minimum-time-to-make-rope-colorful.py
@@ -0,0 +1,8 @@
+    def minCost(self, s, cost):
+        res = max_cost = 0
+        for i in xrange(len(s)):
+            if i > 0 and s[i] != s[i - 1]:
+                max_cost = 0
+            res += min(max_cost, cost[i])
+            max_cost = max(max_cost, cost[i])
+        return res
